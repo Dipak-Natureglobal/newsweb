759d4a95a5815734ceffa1f562dd27e34f32b95b
Dipak-Natureglobal <155736024+Dipak-Natureglobal@users.noreply.github.com>
2025-03-30 20:24:04 +0530

    sign up page completed (#3)

--- src/components/signin-page.jsx
+++ src/components/signin-page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import font from '../images/news.jpg'
 import logo from '../videos/singup.gif'
+import googleIcon from '../images/gooleIcon.png'
 import { signInWithPopup } from 'firebase/auth'
 import { auth, googleProvider } from "../firebase/setup"
+import dayjs from 'dayjs';
 
 function SingIn() {
     const googleSignIn = async () => {
@@ -12,29 +14,50 @@ function SingIn() {
         catch (err) {
             console.log(err)
         }
-       
+
     }
     console.log(auth)
     return (
 
-        <div className="grid grid-cols-1 md:grid-cols-2 bg-[#0d1114] h-screen">
-        <div className="text-center">
-            <img src={logo} className="h-20 mx-auto mt-32" />
-            <h1 className="text-white text-3xl font-semibold mt-7">Sign In</h1>
-            <button 
-                onClick={googleSignIn} 
-                type="button" 
-                className="mt-14 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 h-14 xl:w-96 lg:w-96 w-48">
-                Sign In
-            </button>
-            <h2 className="text-blue-600 underline mt-7">Sign In Now</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 bg-[#0d1114] min-h-[100vh] relative">
+      <div className="text-center">
+        <marquee direction="left" className="text-lg font-semibold bg-gradient-to-r text-white/70 mt-0">
+          Stay updated with the latest news, connect with others, share your thoughts in real-time, and be part of the conversation!
+        </marquee>
+        <img src={logo}  className="h-24 mx-auto mt-32"/>
+        <h1 className="text-white/60 text-3xl font-semibold mt-5">InsightPress</h1>
+        <p className="text-white/60 text-lg">Get the Headlines. Share Your Thoughts.</p>
+        <p className="text-white/60 text-xs">Where News Meets Conversation.</p>
+        <button
+          onClick={googleSignIn}
+          type="button"
+          className="mt-14 text-lg text-gray-900 bg-gradient-to-r from-red-300 via-red-300 to-yellow-300 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-200 font-bold rounded-lg px-5 py-2.5 text-center me-2 mb-2 h-14 xl:w-96 lg:w-96 w-48"
+        >
+          Sign up
+        </button>
+        <span className="flex items-center justify-center space-x-2 mt-7 flex-col xl:flex-row lg:flex-row mb-36">
+          <img src={googleIcon} width={50} height={50} className="xl:my-0 lg:my-1 my-2" />
+          <h2 className="text-white/60">
+            Already have an account?
+            <strong onClick={googleSignIn} className="text-blue-700 underline cursor-pointer mx-1">Sign in</strong>
+          </h2>
+        </span>
+      </div>
+
+      <div className="hidden md:flex justify-end">
+        <img src={font} alt="" className="h-screen object-cover" />
+      </div>
+
+      {/* Footer */}
+      <div className="fixed bottom-0 left-0 text-white/60 text-xs p-4 w-full bg-[#0d1114] z-10">
+        <div className="flex items-start justify-start space-x-2">
+          <p>Copyright © {dayjs().year()} Dipak Mourya</p>
+          <p>•</p>
+          <p>Developed by <a href="https://dipakdev.in" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-500">Dipak</a></p>
         </div>
-    
-        <div className="hidden md:flex justify-end">
-            <img src={font} alt="" className="h-screen object-cover" />
-        </div>
+      </div>
     </div>
-    
+
     )
 }
 
