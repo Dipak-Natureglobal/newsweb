a2def8ba90ac16e60ce1d8406cf049c43fccde2f
Dipak-Natureglobal <dipak.mourya@natureglobal.com>
2025-03-31 02:13:00 +0530

    navbar and signup page completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes , Route} from 'react-router-dom';
 import './App.css';
+import Main from './components/home';
 import SingIn from './components/signin-page'
+
 
 function App() {
   return (
     <h1 className=''>
-      <SingIn />
+      <Routes>
+        <Route path='/singin' element={<SingIn />}/>
+        <Route  path='/' element={<Main />}/>
+      </Routes>
     </h1>    
   );
 }
