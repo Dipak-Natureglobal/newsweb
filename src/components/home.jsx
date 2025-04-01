adecc9e932b6dd3aa02263bcb7c7eabf5b68a656
Dipak-Natureglobal <dipak.mourya@natureglobal.com>
2025-04-01 10:53:26 +0530

    updated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,14 +1,25 @@
 import React, { useCallback, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
-import { CircularProgress } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import { CircularProgress, InputAdornment, TextField } from "@mui/material";
+import NoData from "../images/noData.svg"
 
 function Home(props) {
 
     const [newsData, setNewsData] = useState([]);
-    const [loading, setLoading] = useState(true); // State to track loading status
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredNews = newsData?.filter((data) =>
+        data.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     const getNews = useCallback(() => {
-        setLoading(true); // Set loading to true when fetching starts
+        setLoading(true);
         fetch(`https://newsapi.org/v2/everything?q=${props?.menu ? props.menu : "default"}&sortBy=popularity&apiKey=4e07be5763c44a839f58df9c4f8f7ede`)
             .then(res => res.json())
             .then(json => {
@@ -25,40 +36,100 @@ function Home(props) {
     }, [getNews]);
 
     return (
-        <div className="bg-white/70 mt-36 mb-16 mx-auto px-4 sm:px-6 lg:px-[6rem] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 min-h-[80vh]">
-    {loading ? (  
-        <div className="col-span-3 flex justify-center items-center xl:mt-[15rem] lg:mt-[15rem] mt-[20rem]">
-            <div className="text-center">
-                <CircularProgress />
-                <p className="font-bold">Loading...</p>
+        <div className="bg-white/70 mt-36 mb-16 mx-auto px-4 sm:px-6 lg:px-[6rem]">
+            {!loading && (
+                <div className="flex justify-center mb-4 ">
+                    <TextField
+                        variant="outlined"
+                        placeholder="Search News..."
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        fullWidth
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                        className="max-w-md rounded-lg shadow-md"
+                        sx={{
+                            "& .MuiOutlinedInput-root": {
+                                borderRadius: "20px",
+                            },
+                            "& .MuiInputAdornment-root": {
+                                color: "#1976d2", // Customize the icon color if desired
+                            },
+                            "& .MuiOutlinedInput-notchedOutline": {
+                                borderColor: "#1976d2", // Border color of the input field
+                            },
+                        }}
+                    />
+                </div>
+            )}
+
+            {/* News Content */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 min-h-[80vh]">
+                {loading ? (
+                    <div className="col-span-3 flex justify-center items-center xl:mt-[15rem] lg:mt-[15rem] mt-[20rem]">
+                        <div className="text-center">
+                            <CircularProgress />
+                            <p className="font-bold">Loading...</p>
+                        </div>
+                    </div>
+                ) : (
+                    // If filtered news is empty, show a "No Data Found" message
+                    filteredNews.length === 0 ? (
+                        <div className="col-span-3 text-center">
+                            <img src={NoData} alt="No Data Found" width={450} height={450} className="mx-auto mb-2" />
+                            <p className="text-xl font-semibold">No data found</p>
+                        </div>
+                    ) : (
+
+                        filteredNews && filteredNews.map((data, index) => (
+                            <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                                <img
+                                    className="rounded-t-lg w-full h-48 object-cover"
+                                    src={data.urlToImage}
+                                    alt={data.title}
+                                />
+                                <div className="p-5">
+                                    <a href={data.url}>
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                            {data?.title ?? ""}
+                                        </h5>
+                                    </a>
+                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                        {data?.content ?? ""}
+                                    </p>
+                                    <a
+                                        href={data.url}
+                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >
+                                        Read more
+                                        <svg
+                                            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 14 10"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M1 5h12m0 0L9 1m4 4L9 9"
+                                            />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        ))
+                    )
+                )}
             </div>
         </div>
-    ) : (
-        newsData && newsData.map((data, index) => {
-            return (
-                <>
-                
-                <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-                    <img className="rounded-t-lg w-full h-48 object-cover" src={data.urlToImage} alt={data.title} />
-                    <div className="p-5">
-                        <a href={data.url}>
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data?.title ?? ""}</h5>
-                        </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{data?.content ?? ""}</p>
-                        <a href={data.url} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-                </>
-            );
-        })
-    )}
-</div>
-
     );
 }
 Home.propTypes = {
