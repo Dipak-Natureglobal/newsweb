702080b49933bd72e50d8a93bad2162d2735d8cd
Dipak-Natureglobal <dipak.mourya@natureglobal.com>
2025-04-01 23:02:49 +0530

    re-direct to home page if wrong routing eneter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { Routes , Route} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Main from './components/main';
-import SingIn from './components/signin-page'
-
+import SingIn from './components/signin-page';
+import React from 'react';
 
 function App() {
+  const navigate = useNavigate();
+
+  const NotFoundRedirect = () => {
+    React.useEffect(() => {
+      navigate('/'); 
+    }, []); 
+
+    return null; 
+  };
+
   return (
-    <h1 className=''>
+    <div>
       <Routes>
-        <Route path='/singin' element={<SingIn />}/>
-        <Route  path='/' element={<Main />}/>
+        {/* Define normal routes */}
+        <Route path="/singin" element={<SingIn />} />
+        <Route path="/" element={<Main />} />
+
+        {/* Add wildcard route to handle 404 errors */}
+        <Route path="*" element={<NotFoundRedirect />} />
       </Routes>
-    </h1>    
+    </div>
   );
 }
+
 
 export default App;
